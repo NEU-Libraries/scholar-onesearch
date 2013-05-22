8b2aee8a4825a93ed71c5cc542a5c0eef7c98e6d
Steven Bassett <s.bassett@neu.edu>
2013-05-22 09:20:18 -0400

    Adding styles to the navbar and background to make them more like our current website. Gives a better brand look.

--- js/nulib.js
+++ js/nulib.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('body').addClass('container'); 
+  $('#contentEXL').addClass('container'); 
   $('meta[name="viewport"]').attr('content','width=device-width, initial-scale=1.0');
   $('#search_field').attr('placeholder','Search...');
 });
