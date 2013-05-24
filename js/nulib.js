9d0f2ca7074241928024bacb8118597d266f13cf
Steven Bassett <s.bassett@neu.edu>
2013-05-24 10:55:00 -0400

    Working some more on Pagination, adding a container class to the other divs after body.

--- js/nulib.js
+++ js/nulib.js
@@ -1,5 +1,7 @@
 $(function() {
-  $('#contentEXL').addClass('container'); 
+  //$('#contentEXL').addClass('container'); 
   $('meta[name="viewport"]').attr('content','width=device-width, initial-scale=1.0');
   $('#search_field').attr('placeholder','Search...');
+  $('a.EXLBriefResultsPaginationLinkNext > img').after('<i class="icon-circle-arrow-right"></i>');
+  $('a.EXLBriefResultsPaginationLinkPrevious:first-of-type > img').after('<i class="icon-circle-arrow-left"></i>');
 });
