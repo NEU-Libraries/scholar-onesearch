e08585df8d64acb8dd512d6c62c6a419abe5f132
Steven Bassett <s.bassett@neu.edu>
2013-05-24 14:30:26 -0400

    Adding javascript for the checkbox icons

--- js/nulib.js
+++ js/nulib.js
@@ -8,5 +8,16 @@ $(function() {
   //just adding a special character to the submit button;
   $('#goButton').val("➜");
   $('form[name="rssForm"]').prepend('<i class="icon-rss"></i>');
+  $('img[alt="Add to e-Shelf"]').after('<i class="icon-check-empty icon-large"></i>').hide();
+  $('img[alt="In e-Shelf"]').after('<i class="icon-check icon-large"></i>').hide();
+  $('td.EXLMyShelfStar > a').click(function(){
+    var $i  = $(this).children("i");
+    if ($i.hasClass('icon-check-empty')){
+      $i.removeClass('icon-check-empty').addClass('icon-check');
+    }
+    else{
+      $i.removeClass('icon-check').addClass('icon-check-empty');
+    }
+  });
 
 });
