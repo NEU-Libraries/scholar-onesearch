e50f6e537772f9b711f7fdfbfa1eacd1d6a8164e
Steven Bassett <s.bassett@neu.edu>
2013-05-24 16:22:50 -0400

    Adding tooltips

--- js/nulib.js
+++ js/nulib.js
@@ -10,7 +10,13 @@ $(function() {
   $('form[name="rssForm"]').prepend('<i class="icon-rss"></i>');
   $('img[alt="Add to e-Shelf"]').after('<i class="icon-bookmark-empty icon-large"></i>').hide();
   $('img[alt="In e-Shelf"]').after('<i class="icon-bookmark icon-large"></i>').hide();
-  $('td.EXLMyShelfStar > a').click(function(){
+  $('td.EXLMyShelfStar > a').addClass('btn btn-success btn-small').click(function(){
+    if ($(this).data('original-title') == "In e-Shelf"){
+        $(this).tooltip('destroy').attr('title','Add to e-Shelf').tooltip({title:'Add to e-Shelf'});
+    }
+    else{
+      $(this).tooltip('destroy').attr('title',"In e-Shelf").tooltip({title:'In e-Shelf'});
+    }
     var $i  = $(this).children("i");
     if ($i.hasClass('icon-bookmark-empty')){
       $i.removeClass('icon-bookmark-empty').addClass('icon-bookmark');
@@ -18,6 +24,7 @@ $(function() {
     else{
       $i.removeClass('icon-bookmark').addClass('icon-bookmark-empty');
     }
-  });
-
+  }).tooltip();
+  //tooltips for more of the UI
+  $("a#showMoreOptions, a.EXLSearchFieldRibbonAdvancedTwoLinks").tooltip();
 });
