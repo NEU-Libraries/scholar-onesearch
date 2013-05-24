3d05d1f0d19de9b1309c5b7f684f00975f12d531
Steven Bassett <s.bassett@neu.edu>
2013-05-24 11:49:42 -0400

    Adding panel styles to the results table.

--- js/nulib.js
+++ js/nulib.js
@@ -2,9 +2,11 @@ $(function() {
   //$('#contentEXL').addClass('container'); 
   $('meta[name="viewport"]').attr('content','width=device-width, initial-scale=1.0');
   $('#search_field').attr('placeholder','Search...');
+  //Adding icons for the next arrows
   $('a.EXLBriefResultsPaginationLinkNext > img').after('<i class="icon-circle-arrow-right"></i>');
   $('a.EXLBriefResultsPaginationLinkPrevious:first-of-type > img').after('<i class="icon-circle-arrow-left"></i>');
-  if(window.innerWidth < 500){
-    $('#goButton').val("→");
-  }
+  //just adding a special character to the submit button;
+  $('#goButton').val("➜");
+  
+
 });
