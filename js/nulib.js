7f52dcbf42d859b4273585012280916aea77c4dd
Steven Bassett <s.bassett@neu.edu>
2013-05-29 16:02:25 -0400

    Updateing the JS to add a popover when they are not signed in

--- js/nulib.js
+++ js/nulib.js
@@ -1,7 +1,4 @@
 $(function() {
-  //$('#contentEXL').addClass('container'); 
-  $('meta[name="viewport"]').attr('content','width=device-width, initial-scale=1.0');
-  $('#search_field').attr('placeholder','Search...');
   //Adding icons for the next arrows
   $('a.EXLBriefResultsPaginationLinkNext > img').after('<i class="icon-circle-arrow-right"></i>');
   $('a.EXLBriefResultsPaginationLinkPrevious:first-of-type > img').after('<i class="icon-circle-arrow-left"></i>');
@@ -30,4 +27,28 @@ $(function() {
   //replace the external link icon with icon-font
   $('img[src="../images/icon_popout_tab.png"]').hide().after('<i class="icon-external-link"></i>');
   
+  // Adding the function to crate the popover for the sign in link.
+  var signinPopover  = function(){
+    if ( sessionStorage.getItem('popoverDissmiss') !== 'true'){
+      $('ul.EXLEShelfTileGuest > li.EXLSignOut').popover({
+      title: 'Yo! Sign in <button="#exlidSignOut" id="nulib-signin-popover" class="close pull-right" onclick="dismissSigninPopover()")">x</button>',
+      content: '<p>you better sign in</p>',
+      placement: 'bottom',
+      // trigger: 'manual',
+      html: true,
+      }).popover('show');
+    }
+    else{
+      return;
+    } 
+  }
+
+  signinPopover();
 });
+
+
+
+var dismissSigninPopover = function(){
+  $('ul.EXLEShelfTileGuest > li.EXLSignOut').popover('hide');
+  sessionStorage.setItem('popoverDissmiss', 'true');
+}
