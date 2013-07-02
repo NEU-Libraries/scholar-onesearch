a2d715179fc6ed730e9073ef01f84392ea81b4d9
Steven Bassett <steven.j.bassett@gmail.com>
2013-07-02 17:03:01 -0400

    Restructuing the project for uglifgy and grunt magic!

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,25 +4,36 @@ module.exports = function(grunt) {
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json')
-    // uglify: {
-    //   options: {
-    //     banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
-    //   },
-    //   build: {
-    //     src: 'js/src/<%= pkg.name %>.js',
-    //     dest: 'js/build/<%= pkg.name %>.min.js'
-    //   }
-    // }
 
+    uglify: {/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */
+      },
+      build: {
+        src: 'js/src/<%= pkg.name %>.js',
+        dest: 'js/build/<%= pkg.name %>.min.js'
+      }
+    },
+    recess: {
+        dist: {
+            options: {
+                compile: true,
+                compress: true
+            },
+            files: {
+                'css/style.css': ['less/style.less']
+            }
+        }
+    }
   });
 
   // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-recess');
 
   // Load the plugin that provides recess to compile and lint LESS to CSS
   grunt.loadNpmTasks('grunt-recess');
 
   // Default task(s).
-  grunt.registerTask('default', ['recess']);
+  grunt.registerTask('default', ['uglify','recess']);
+
 
 };
