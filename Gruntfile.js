cf68999d29f73e2ebe126f774ea49bb05f74392a
Steven Bassett <steven.j.bassett@gmail.com>
2013-09-24 12:30:12 -0400

    Moving the font-awesome library into seperate git submodule

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
-          paths: ["less"]
+          paths: ["less"],
         },
         files: {
           "css/style.css": "less/style.less"
