d1c3892e388f3e646654f58cfd74a43dce5204d6
Steven Bassett <steven.j.bassett@gmail.com>
2013-09-25 21:42:43 -0400

    Working on reducing the size of the advanced search ribbon

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,16 +36,6 @@ module.exports = function(grunt) {
         }
       }
     },
-    sftp: {
-        files: {
-            "./css/style.css": "./css/style.css"
-        },
-    },
-    // sshexec: {
-    //   gitPull: {
-    //     command: 'cd <%= this.path %> && git pull --recurse-submodules=yes -v',
-    //   }
-    // }
     sshexec: {
         stagingPull: {
            command: 'cd <%= stagingServer.path %> && git pull -v',
@@ -56,18 +46,6 @@ module.exports = function(grunt) {
            }
          }
        },
-    autoshot: {
-      default_options: {
-        options: {
-          // necessary config
-          path: 'temp/autoshot',
-          type: 'png',
-          // optional config, must set either remote or local
-          remote: 'http://onesearch-test.library.northeastern.edu/primo_library/libweb/action/search.do?vid=NUdev',
-          viewport: ['1024x768', '1920x1080']
-        },
-      },
-    },
   });
 
   // Load the plugin that provides the "uglify" task.
@@ -76,7 +54,6 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-ssh');
-  grunt.loadNpmTasks('grunt-autoshot');
   // Default task(s).
 
   grunt.registerTask('default', ['uglify','less:production']);
