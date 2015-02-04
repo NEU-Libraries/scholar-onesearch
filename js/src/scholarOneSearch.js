185ebfc7a7ead5b906ac17da4951de2ca14c7137
Eli Zoller <zoller.eli@gmail.com>
2015-02-04 15:38:15 -0500

    hiding DRS link and styling for now issue #170

--- js/src/scholarOneSearch.js
+++ js/src/scholarOneSearch.js
@@ -417,7 +417,6 @@ jQuery(function($) {
 
     var addToolTips = function(){
       $("a#showMoreOptions, a.EXLSearchFieldRibbonAdvancedTwoLinks").tooltip();
-      //$("a.EXLMainMenuITEMdrs").attr('title', 'Digital Repository Service').tooltip({ title: "Digital Repository Service"});
     };
 
 
