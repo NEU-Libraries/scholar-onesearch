77b82a798265a6ac52e4bc2f72993ceb74df3507
Eli Zoller <zoller.eli@gmail.com>
2017-01-13 14:05:11 -0500

    for #239 - js for adding both logos

--- NUdev/js/custom.js
+++ NUdev/js/custom.js
@@ -14,6 +14,23 @@
     /****************************************************************************************************/
 
 
-})();
+    /*make lib logo clickable*/
+    app.controller('prmLogoAfterController', [function () {
+      // var vm = this;
+      // vm.getIconLink = getIconLink;
+      // function getIconLink() {
+      //   return vm.parentCtrl.iconLink;
+      // }
+    }]);
 
-   
+    app.component('prmLogoAfter',{
+      bindings: {parentCtrl: '<'},
+      controller: 'prmLogoAfterController',
+      template: '<div class="product-logo product-logo-local" layout="row" layout-align="start center" layout-fill id="banner" tabindex="0" role="banner">' +
+      '<a href="http://library.northeastern.edu">' +
+      '<img class="logo-image" alt="{{::(&apos;nui.header.LogoAlt&apos; | translate)}}" ng-src="custom/NUdev/img/nu-libraries-logo.svg"/></a>' +
+      '<a href="http://onesearch.northeastern.edu">' +
+      '<img class="logo-image sos" alt="{{::(&apos;nui.header.LogoAlt&apos; | translate)}}" ng-src="custom/NUdev/img/sosbold.svg"/></a></div>'
+    });
+    /*end make lib logo clickable*/
+})();
