530b0add5e51f30e1d6e45362375b3c428898bb7
Eli Zoller <zoller.eli@gmail.com>
2017-02-14 09:07:55 -0500

    fixes for when the link isn't present

--- js/src/scholarOneSearch.js
+++ js/src/scholarOneSearch.js
@@ -162,27 +162,38 @@ jQuery(function($) {
 
         /*Build for viewOnline Button to function using ezproxy when external resource being loaded in iframe*/
     var buildViewOnline = function( result ){
-        var viewonline = result.$el.find(".EXLViewOnlineTab.EXLResultTabIconPopout a").attr("href");
-        if (viewonline.indexOf("ezproxy.neu.edu/login") < 0){
-          var newhref = 'http://ezproxy.neu.edu/login?URL=' + viewonline;
-        } else {
-          var newhref = viewonline;
+      var viewonline = result.$el.find(".EXLViewOnlineTab.EXLResultTabIconPopout a");
+      if (viewonline) {
+        viewonline = viewonline.attr("href");
+        if (viewonline != undefined){
+          if (viewonline.indexOf("ezproxy.neu.edu/login") < 0){
+            var newhref = 'http://ezproxy.neu.edu/login?URL=' + viewonline;
+          } else {
+            var newhref = viewonline;
+          }
+          if (newhref.indexOf("search.proquest.com") >= 0) {
+            newhref = newhref + '?accountid=12826';
+          }
+          result.$el.find(".EXLViewOnlineTab.EXLResultTabIconPopout a").attr("href", newhref);
         }
-        if (newhref.indexOf("search.proquest.com") >= 0) {
-          newhref = newhref + '?accountid=12826';
+      }
+
+      var single_viewonline = result.$el.find(".EXLViewOnlineTab.EXLResultSelectedTabEXLResultTabIconPopout a");
+      if (single_viewonline){
+        single_viewonline = single_viewonline.attr("href");
+        if (single_viewonline != undefined){
+          if (single_viewonline.indexOf("http://ezproxy.neu.edu/login") < 0){
+            var single_newhref = 'ezproxy.neu.edu/login?URL=' + single_viewonline;
+          } else {
+            var single_newhref = single_viewonline;
+          }
+          if (single_newhref.indexOf("search.proquest.com") >= 0) {
+            single_newhref = single_newhref + '?accountid=12826';
+          }
+          result.$el.find(".EXLViewOnlineTab.EXLResultSelectedTabEXLResultTabIconPopout a").attr("href", single_newhref);
+          result.$el.find(".EXLContainer-viewOnlineTab .EXLTabHeaderContent a").attr("href", single_newhref);
         }
-        result.$el.find(".EXLViewOnlineTab.EXLResultTabIconPopout a").attr("href", newhref);
-        var single_viewonline = result.$el.find(".EXLViewOnlineTab.EXLResultSelectedTabEXLResultTabIconPopout a").attr(href);
-        if (single_viewonline.indexOf("http://ezproxy.neu.edu/login") < 0){
-          var single_newhref = 'ezproxy.neu.edu/login?URL=' + single_viewonline;
-        } else {
-          var single_newhref = viewonline;
-        }
-        if (single_newhref.indexOf("search.proquest.com") >= 0) {
-          single_newhref = single_newhref + '?accountid=12826';
-        }
-        result.$el.find(".EXLViewOnlineTab.EXLResultSelectedTabEXLResultTabIconPopout a").attr("href", single_newhref);
-        result.$el.find(".EXLContainer-viewOnlineTab .EXLTabHeaderContent a").attr("href", single_newhref);
+      }
     }
 
     function buildFindIt(result) {
