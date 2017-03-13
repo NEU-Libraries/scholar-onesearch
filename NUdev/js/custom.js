a3562d12821fb0baffcc3137b0fefb8d9461ec9c
Eli Zoller <zoller.eli@gmail.com>
2017-03-13 15:27:27 -0400

    Add google analytics tracking

--- NUdev/js/custom.js
+++ NUdev/js/custom.js
@@ -1,3 +1,8 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
 (function () {
     "use strict";
     'use strict';
@@ -234,4 +239,13 @@
       template: '<md-button class="md-primary" ng-click="$ctrl.journalTitleSearch()" id="journal_title_search" aria-label="Journal Title Search">Journal Title Search</md-button>'
     });
     /*ends journal title search link to advanced search*/
+
+    /*add google analytics*/
+    app.run(['$rootScope', '$location', '$window', function($rootScope, $location, $window){
+      $window.ga('create', 'UA-4426028-9', 'auto');
+      $rootScope.$on('$locationChangeSuccess', function(event){
+        $window.ga('send', 'pageview', {location: $location.url()});
+      });
+    }]);
+    /*end add google anlytics*/
 })();
