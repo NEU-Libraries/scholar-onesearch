b937b8c9da4f01d9cbe33493a083aff2ae46e04a
gam5 <g.mcclellan@northeastern.edu>
2017-06-09 14:05:15 -0400

    code to move virtual browse section
    taken from https://github.com/muratseyhan/primo-explore-section-ordering

---
+++ NUdev/js/custom.module.js
@@ -0,0 +1,3 @@
+// This package assumes that a 'viewCustom' module is defined in your application.
+// Uncomment the following line if you want to run this package directly.  
+angular.module('viewCustom', []);
